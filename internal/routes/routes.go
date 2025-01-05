9ff3b276f05aec2d73172fdf4518aa5624df9f02
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-01-05 15:33:03 +0700

    fix(internal): updating the route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,4 +9,5 @@ import (
 func SetupRoutes(app *fiber.App, urlHandler *handler.URLHandler) {
     app.Post("/shorten", urlHandler.ShortenURL)
     app.Get("/:shortURL", urlHandler.ResolveURL)
+	app.Get("/", urlHandler.GetAllURLs)
 }
